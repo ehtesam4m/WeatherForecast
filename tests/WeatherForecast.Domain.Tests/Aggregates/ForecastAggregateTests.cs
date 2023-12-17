bf8d6072b6ca6a5e465ff9e0ca1a1b0def48bfbc
ehtesam.ahmed <ehtesam.ahmed@metrixlab.com>
2023-12-17 12:43:43 +0100

    domain tests are added

---
+++ tests/WeatherForecast.Domain.Tests/Aggregates/ForecastAggregateTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecast.Domain.Aggregates.Forecast;
+using WeatherForecast.Domain.Aggregates.Forecast.Events;
+
+namespace WeatherForecast.Domain.Tests.Aggregates
+{
+    public class ForecastAggregateTests
+    {
+        [Fact]
+        public void WhenDateIsInThePast_ForecastCreation_ShouldThrowException()
+        {
+            var action = () => { new Forecast(DateOnly.FromDateTime(DateTime.Now.AddDays(-2)), 50); };
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void WhenTemperatureLessThanMinus60_ForecastCreation_ShouldThrowException()
+        {
+            var action = () => { new Forecast(DateOnly.FromDateTime(DateTime.Now), -61); };
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void WhenTemperatureGreaterThan60_ForecastCreation_ShouldThrowException()
+        {
+            var action = () => { new Forecast(DateOnly.FromDateTime(DateTime.Now), 61); };
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void WhenParametersAreValid_ForecastCreation_ShouldRaiseForecastCreatedEvent()
+        {
+            var forecast = new Forecast(DateOnly.FromDateTime(DateTime.Now), 50);
+            
+            forecast.DomainEvents.Count().Should().Be(1);
+            forecast.DomainEvents.First().GetType().Should().Be(typeof(ForecastCreatedEvent));
+            
+        }
+    }
+}
