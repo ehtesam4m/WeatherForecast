de1a9ec4f772f945279e85a0200fba95ad622e13
ehtesam.ahmed <ehtesam.ahmed@metrixlab.com>
2023-12-17 11:56:24 +0100

    aggreed on approach

--- tests/WeatherForecast.Application.Tests/Command/CreateForecastCommandHandlerTests.cs
+++ tests/WeatherForecast.Application.Tests/Command/CreateForecastCommandHandlerTests.cs
@@ -31,27 +31,20 @@ namespace WeatherForecast.Application.Tests.Command
                             .With(x => x.Date, DateOnly.FromDateTime(DateTime.Now))
                             .With(x => x.Temperature, 5)
                             .Create();
-            
-            Forecast savedAggregate = null;
-            _repository.Setup(x => x.CreateAsync(It.IsAny<Forecast>(), It.IsAny<CancellationToken>()))
-                .Callback((Forecast aggregate, CancellationToken cancellationToken) =>
-                {
-                    savedAggregate = aggregate;
-                }
-                ).Returns(Task.CompletedTask);
-
-            _unitOfWork.Setup(x => x.CompleteAsync()).Returns(Task.CompletedTask);
 
             var sut = new CreateForecastCommandHandler(_repository.Object, _unitOfWork.Object);
             await sut.Handle(command, It.IsAny<CancellationToken>());
 
+            _repository.Verify(x => x.CreateAsync(It.Is<Forecast>(x => x.Date == command.Date 
+            && x.Temperature == command.Temperature), 
+            It.IsAny<CancellationToken>()), Times.Once);
+
             _unitOfWork.Verify(x => x.CompleteAsync(), Times.Once);
-            savedAggregate.Date.Should().Be(command.Date);
-            savedAggregate.Temperature.Should().Be(command.Temperature);
+ 
         }
 
         [Theory, CustomAutoData]
-        public async Task WhenDateAlreadyExists_Handler_ShouldReurnInvalidOperationException(CreateForecastCommand command, Forecast forecastff)
+        public async Task WhenDateAlreadyExists_Handler_ShouldReurnInvalidOperationException(CreateForecastCommand command)
         {
             _repository.Setup(x => x.GetForecastByDate(command.Date)).ReturnsAsync(new ForecastBuilder().Build());
             
